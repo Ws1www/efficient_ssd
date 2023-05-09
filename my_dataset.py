8515491af60855a03f97a303e42f17d3d730c8a3
Ws1www <129833341+Ws1www@users.noreply.github.com>
2023-05-09 20:56:49 +0800

    Update my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -1,82 +1,137 @@
+from torch.utils.data import Dataset
 import os
 import torch
-from torch.utils import data
-from pathlib import Path
+import json
 from PIL import Image
-
-import numpy as np
-
-
-class YOLODataset(data.Dataset):
-    '''
-    读取YOLO 数据格式的数据集
-    数据标注：1->circle, 2->Yes, 3->No
-    '''
-    def __init__(self, root, transforms=None, is_train=True): # 数据集文件夹camera2/images 包含训练集合和测试集
-        super().__init__()
-        if is_train:
-            # root = '/kaggle/input/diordata/'
-            self.img_path = Path(root) / 'images'
-            self.lable_path = Path(root) / 'labels'
-            with open(str(Path(root) / 'ImageSets/train.txt'), 'r') as f:
-                x = f.readlines()
-            self.dataname = [i.strip() for i in x]
+from lxml import etree
 
 
-        else:
-            self.img_path = Path(root) / 'images'
-            self.lable_path = Path(root) / 'labels'
-            with open(str(Path(root) / 'ImageSets/val.txt'), 'r') as f:
-                x = f.readlines()
-            self.dataname = [i.strip() for i in x]
+class VOCDataSet(Dataset):
+    """读取解析PASCAL VOC2007/2012数据集"""
+
+    def __init__(self, voc_root, transforms=None, train_set='train.txt'):
+        # assert year in ["2007", "2012"], "year must be in ['2007', '2012']"
+        # 增加容错能力
+        # if "VOCdevkit" in voc_root:
+        #     self.root = os.path.join(voc_root, f"VOC{year}")
+        # else:
+        #     self.root = os.path.join(voc_root, "VOCdevkit", f"VOC{year}")
+        self.root = voc_root
+        self.img_root = os.path.join(self.root, "Imgs-trainval", 'JPEGImages-trainval')
+        self.annotations_root = os.path.join(self.root, "Annotations")
+
+        txt_list = os.path.join(self.root, "ImageSets", "Main", train_set)
+
+        with open(txt_list) as read:
+            self.xml_list = [os.path.join(self.annotations_root, line.strip() + ".xml")
+                             for line in read.readlines() if len(line.strip()) > 0]
+
+        # read class_indict
+        json_file = "./pascal_voc_classes.json"
+        assert os.path.exists(json_file), "{} file not exist.".format(json_file)
+        with open(json_file, 'r') as f:
+            self.class_dict = json.load(f)
+
         self.transforms = transforms
 
     def __len__(self):
-        return len(self.dataname)
-    
-    def __getitem__(self, index):
-        image = Image.open(str(self.img_path / (self.dataname[index] + '.jpg')))
-        data_width, data_height = image.size
+        return len(self.xml_list)
+
+    def __getitem__(self, idx):
+        # read xml
+        xml_path = self.xml_list[idx]
+        with open(xml_path) as fid:
+            xml_str = fid.read()
+        xml = etree.fromstring(xml_str)
+        data = self.parse_xml_to_dict(xml)["annotation"]
+        data_height = int(data["size"]["height"])
+        data_width = int(data["size"]["width"])
         height_width = [data_height, data_width]
+        img_path = os.path.join(self.img_root, data["filename"])
+        image = Image.open(img_path)
+        if image.format != "JPEG":
+            raise ValueError("Image '{}' format not JPEG".format(img_path))
 
-        # print(self.dataname[index])
+        assert "object" in data, "{} lack of object information.".format(xml_path)
+        boxes = []
+        labels = []
+        iscrowd = []
+        for obj in data["object"]:
+            # 将所有的gt box信息转换成相对值0-1之间
+            xmin = float(obj["bndbox"]["xmin"]) / data_width
+            xmax = float(obj["bndbox"]["xmax"]) / data_width
+            ymin = float(obj["bndbox"]["ymin"]) / data_height
+            ymax = float(obj["bndbox"]["ymax"]) / data_height
 
-        # read txt
-        boxes, labels = [], []
-        with open(str(self.lable_path / (self.dataname[index] + '.txt')), 'r') as f:
-            for line in f.readlines():
-                x = [float(i) for i in line.split(' ')]
-                boxes.append([x[1:]])
-                labels.append(int(x[0]) + 1)
-        
-        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
-        xmin = boxes[:, 0] - 0.5 * boxes[:, 2]
-        ymin = boxes[:, 1] - 0.5 * boxes[:, 3]
-        xmax = boxes[:, 0] + 0.5 * boxes[:, 2]
-        ymax = boxes[:, 1] + 0.5 * boxes[:, 3]
-        boxes[:, 0] = xmin
-        boxes[:, 1] = ymin
-        boxes[:, 2] = xmax
-        boxes[:, 3] = ymax
-        # print(boxes)
+            # 进一步检查数据，有的标注信息中可能有w或h为0的情况，这样的数据会导致计算回归loss为nan
+            if xmax <= xmin or ymax <= ymin:
+                print("Warning: in '{}' xml, there are some bbox w/h <=0".format(xml_path))
+                continue
+                
+            boxes.append([xmin, ymin, xmax, ymax])
+            labels.append(self.class_dict[obj["name"]])
+            # if "difficult" in obj:
+            #     iscrowd.append(int(obj["difficult"]))
+            # else:
+            #     iscrowd.append(0)
+            iscrowd.append(0)
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
         labels = torch.as_tensor(labels, dtype=torch.int64)
+        iscrowd = torch.as_tensor(iscrowd, dtype=torch.int64)
         height_width = torch.as_tensor(height_width, dtype=torch.int64)
-        image_id = torch.tensor([index])
+        image_id = torch.tensor([idx])
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        
-        target = {}
-        target['boxes'] = boxes
-        target['labels'] = labels
-        target['image_id'] = image_id
-        target['area'] = area
-        target['height_width'] = height_width
 
-        # print(self.dataname[index])
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+        target["height_width"] = height_width
+
         if self.transforms is not None:
             image, target = self.transforms(image, target)
+
         return image, target
-    
-    def coco_index(self, index):
+
+    def get_height_and_width(self, idx):
+        # read xml
+        xml_path = self.xml_list[idx]
+        with open(xml_path) as fid:
+            xml_str = fid.read()
+        xml = etree.fromstring(xml_str)
+        data = self.parse_xml_to_dict(xml)["annotation"]
+        data_height = int(data["size"]["height"])
+        data_width = int(data["size"]["width"])
+        return data_height, data_width
+
+    def parse_xml_to_dict(self, xml):
+        """
+        将xml文件解析成字典形式，参考tensorflow的recursive_parse_xml_to_dict
+        Args：
+            xml: xml tree obtained by parsing XML file contents using lxml.etree
+
+        Returns:
+            Python dictionary holding XML contents.
+        """
+
+        if len(xml) == 0:  # 遍历到底层，直接返回tag对应的信息
+            return {xml.tag: xml.text}
+
+        result = {}
+        for child in xml:
+            child_result = self.parse_xml_to_dict(child)  # 递归遍历标签信息
+            if child.tag != 'object':
+                result[child.tag] = child_result[child.tag]
+            else:
+                if child.tag not in result:  # 因为object可能有多个，所以需要放入列表里
+                    result[child.tag] = []
+                result[child.tag].append(child_result[child.tag])
+        return {xml.tag: result}
+
+    def coco_index(self, idx):
         """
         该方法是专门为pycocotools统计标签信息准备，不对图像和标签作任何处理
         由于不用去读取图片，可大幅缩减统计时间
@@ -84,46 +139,50 @@ class YOLODataset(data.Dataset):
         Args:
             idx: 输入需要获取图像的索引
         """
-        image = Image.open(str(self.img_path / (self.dataname[index] + '.jpg')))
-        data_width, data_height = image.size
+        # read xml
+        xml_path = self.xml_list[idx]
+        with open(xml_path) as fid:
+            xml_str = fid.read()
+        xml = etree.fromstring(xml_str)
+        data = self.parse_xml_to_dict(xml)["annotation"]
+        data_height = int(data["size"]["height"])
+        data_width = int(data["size"]["width"])
         height_width = [data_height, data_width]
-
-
-        # read txt
-        boxes, labels = [], []
-        with open(str(self.lable_path / (self.dataname[index] + '.txt')), 'r') as f:
-            for line in f.readlines():
-                x = [float(i) for i in line.split(' ')]
-                boxes.append([x[1:]])
-                labels.append(int(x[0]) + 1)
-        
-        # iscrowd = torch.as_tensor([0, 0, 0], dtype=torch.int64)
-        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
-        xmin = boxes[:, 0] - 0.5 * boxes[:, 2]
-        ymin = boxes[:, 1] - 0.5 * boxes[:, 3]
-        xmax = boxes[:, 0] + 0.5 * boxes[:, 2]
-        ymax = boxes[:, 1] + 0.5 * boxes[:, 3]
-        boxes[:, 0] = xmin
-        boxes[:, 1] = ymin
-        boxes[:, 2] = xmax
-        boxes[:, 3] = ymax
-
-        iscrowd = torch.zeros(len(boxes))
+        # img_path = os.path.join(self.img_root, data["filename"])
+        # image = Image.open(img_path)
+        # if image.format != "JPEG":
+        #     raise ValueError("Image format not JPEG")
+        boxes = []
+        labels = []
+        iscrowd = []
+        for obj in data["object"]:
+            # 将所有的gt box信息转换成相对值0-1之间
+            xmin = float(obj["bndbox"]["xmin"]) / data_width
+            xmax = float(obj["bndbox"]["xmax"]) / data_width
+            ymin = float(obj["bndbox"]["ymin"]) / data_height
+            ymax = float(obj["bndbox"]["ymax"]) / data_height
+            boxes.append([xmin, ymin, xmax, ymax])
+            labels.append(self.class_dict[obj["name"]])
+            # iscrowd.append(int(obj["difficult"]))
+            iscrowd.append(0)
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
         labels = torch.as_tensor(labels, dtype=torch.int64)
+        iscrowd = torch.as_tensor(iscrowd, dtype=torch.int64)
         height_width = torch.as_tensor(height_width, dtype=torch.int64)
-        image_id = torch.tensor([index])
+        image_id = torch.tensor([idx])
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        
+
         target = {}
-        target['boxes'] = boxes
-        target['labels'] = labels
-        target['image_id'] = image_id
-        target['area'] = area
-        target['height_width'] = height_width
-        target['iscrowd'] = iscrowd
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+        target["height_width"] = height_width
 
         return target
-    
+
     @staticmethod
     def collate_fn(batch):
         images, targets = tuple(zip(*batch))
@@ -141,27 +200,3 @@ class YOLODataset(data.Dataset):
         #            "image_id": torch.as_tensor(img_id)}
 
         return images, targets
-    
-
-if __name__ == '__main__':
-    # import transforms
-    # os.system('clear')
-    # data_transform = {
-    #     'train':transforms.Compose([transforms.SSDCropping(),
-    #                                 transforms.Resize(),
-    #                                 transforms.ColorJitter(),
-    #                                 transforms.ToTensor(),
-    #                                 transforms.RandomHorizontalFlip(),
-    #                                 transforms.Normalization(),
-    #                                 transforms.AssignGTtoDefaultBox()]),
-    #     'val':transforms.Compose([transforms.Resize(),
-    #                               transforms.ToTensor(),
-    #                               transforms.Normalization()])}
-    # x = YOLODataset('../camera', transforms=data_transform['train'])
-    # for image, target in x:
-    #     # image
-    #     print(target)
-    #     print(target['labels'].shape)
-    #     print(target['labels'].max())
-    #     break
-    x = YOLODataset('dior')
