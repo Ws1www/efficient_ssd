0df051f1c420bf0d85b4b95d05056a4985e4acb9
Ws1www <2536077115@qq.com>
2023-05-09 18:20:33 +0800

    efficient

---
+++ plot_curve.py
@@ -0,0 +1,61 @@
+import datetime
+import matplotlib.pyplot as plt
+
+
+def plot_loss_and_lr(train_loss, learning_rate):
+    try:
+        x = list(range(len(train_loss)))
+        fig, ax1 = plt.subplots(1, 1)
+        ax1.plot(x, train_loss, 'r', label='loss')
+        ax1.set_xlabel("epoch")
+        ax1.set_ylabel("loss")
+        ax1.set_title("Train Loss and lr")
+        plt.legend(loc='best')
+
+        ax2 = ax1.twinx()
+        ax2.plot(x, learning_rate, label='lr')
+        ax2.set_ylabel("learning rate")
+        ax2.set_xlim(0, len(train_loss))  # 设置横坐标整数间隔
+        plt.legend(loc='best')
+
+        handles1, labels1 = ax1.get_legend_handles_labels()
+        handles2, labels2 = ax2.get_legend_handles_labels()
+        plt.legend(handles1 + handles2, labels1 + labels2, loc='upper right')
+
+        fig.subplots_adjust(right=0.8)  # 防止出现保存图片显示不全的情况
+        fig.savefig('./loss_and_lr{}.png'.format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
+        plt.close()
+        print("successful save loss curve! ")
+    except Exception as e:
+        print(e)
+
+
+def plot_map(mAP):
+    try:
+        x = list(range(len(mAP)))
+        plt.plot(x, mAP, label='mAp')
+        plt.xlabel('epoch')
+        plt.ylabel('mAP')
+        plt.title('Eval mAP')
+        plt.xlim(0, len(mAP))
+        plt.legend(loc='best')
+        plt.savefig('./mAP.png')
+        plt.close()
+        print("successful save mAP curve!")
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    train_loss, lr = [], []
+    mAP = []
+    with open(r'30lun.txt', 'r') as f:
+        for line in f.readlines():
+            print(line)
+            x = line.split('  ')
+            mAP.append(float(x[1]))
+            train_loss.append(float(x[-2]))
+            lr.append(float(x[-1]))
+
+    plot_map(mAP)
+    plot_loss_and_lr(train_loss, lr)
+    
